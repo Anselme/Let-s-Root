3fae103941336a466c88de9c2bed87d7cb77a176
Stéphane Girault <stephane@letsroot.net>
2014-05-14 17:42:38 +0200

    système anti-spam

--- contact.php
+++ contact.php
@@ -8,6 +8,9 @@ $name = $_POST['name'];
 $courriel = $_POST['email'];
 $subject = $_POST['subject']. " ... un message du formulaire de contact de Letsroot !";
 $message = wordwrap($_POST['message'], 70, "\r\n");
+$spam = $_POST['tel'];
+
+if($spam == ''){
 
 $body = <<<EOD
     Nom: $name \n\n
@@ -16,9 +19,10 @@ $body = <<<EOD
     Message:\n\n $message
 EOD;
 
-$headers = 'From: Let\'s Root <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $courriel;
+    $headers = 'From: Let\'s Root <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $courriel;
 
-$mailSent = mail($emailTo, $subject, $body, $headers);
+    $mailSent = mail($emailTo, $subject, $body, $headers);
+}
 
 $mailSent ? $result = "ok" : $result = "ko";
 
